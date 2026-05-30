613fa849733c41f0b009c3c60eaa74ef02cc4169
cfc310186-gif <cfc310186@gmail.com>
2026-05-30 21:11:52 +0800

    Clear stale web caches before Flutter startup

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -2,6 +2,10 @@
 {{flutter_build_config}}
 
 (async function () {
+  if (window.assetTrackerCacheCleanup) {
+    await window.assetTrackerCacheCleanup;
+  }
+
   if ('serviceWorker' in navigator) {
     const registrations = await navigator.serviceWorker.getRegistrations();
     await Promise.all(registrations.map((registration) => registration.unregister()));
