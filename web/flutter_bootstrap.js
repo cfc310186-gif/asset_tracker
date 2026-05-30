7164f9b789c279fb9481fb39987e7399761a987d
cfc310186-gif <cfc310186@gmail.com>
2026-05-30 18:32:42 +0800

    Unregister stale Flutter service workers

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,4 +1,11 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-_flutter.loader.load();
+(async function () {
+  if ('serviceWorker' in navigator) {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    await Promise.all(registrations.map((registration) => registration.unregister()));
+  }
+
+  await _flutter.loader.load();
+})();
